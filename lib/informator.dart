afc860283923d4dd7d3b7fe25db3e9c95f120fa2
Ernest Aitiev <valeetmihifaveas@gmail.com>
2025-03-13 21:10:16 +0600

    Init commit with http dependency and main widget

---
+++ lib/informator.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CountryInformator extends StatefulWidget {
+  const CountryInformator({super.key});
+
+  @override
+  State<CountryInformator> createState() => _CountryInformatorState();
+}
+
+class _CountryInformatorState extends State<CountryInformator> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
